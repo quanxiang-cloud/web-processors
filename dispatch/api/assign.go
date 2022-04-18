85477582f73f553abe94229c8d93edde2405f33c
minhjh <mingmj7557@gmail.com>
2022-04-18 16:07:29 +0800

    feature: add command scheduling rule

---
+++ dispatch/api/assign.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/quanxiang-cloud/cabin/logger"
+	"github.com/quanxiang-cloud/cabin/tailormade/header"
+	"github.com/quanxiang-cloud/cabin/tailormade/resp"
+	"github.com/quanxiang-cloud/web-processors/dispatch/internal/chain"
+)
+
+// Execute is the main function of assign.
+func Execute(cmd chain.Command) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := header.MutateContext(c)
+
+		args := &chain.Arg{}
+		if err := c.ShouldBind(args); err != nil {
+			logger.Logger.WithName("Bind Parma").Errorw(err.Error(), header.GetRequestIDKV(ctx).Fuzzy()...)
+			resp.Format(nil, err).Context(c, http.StatusBadRequest)
+		}
+
+		if err := cmd.Do(ctx, args); err != nil {
+			resp.Format(nil, err).Context(c, http.StatusInternalServerError)
+		}
+
+		resp.Format(nil, nil).Context(c, http.StatusOK)
+	}
+}
---
+++ dispatch/internal/chain/css.go
@@ -0,0 +1,47 @@
+package chain
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os/exec"
+
+	"github.com/quanxiang-cloud/cabin/logger"
+	"github.com/quanxiang-cloud/cabin/tailormade/header"
+	"github.com/quanxiang-cloud/web-processors/dispatch/pkg/config"
+)
+
+type css struct {
+	conf *config.Config
+	next Command
+}
+
+// NewCSS css.
+func NewCSS(conf *config.Config) *css {
+	return &css{
+		conf: conf,
+		next: NewFileserver(conf),
+	}
+}
+
+func (c *css) Name() string {
+	return "css"
+}
+
+func (c *css) Do(ctx context.Context, args *Arg) error {
+	var (
+		stderr      bytes.Buffer
+		commandPath = fmt.Sprintf("%s/%s", c.conf.CommandDir, c.Name())
+	)
+
+	cmd := exec.Command(commandPath)
+	cmd.Stderr = &stderr
+
+	if _, err := cmd.Output(); err != nil {
+		logger.Logger.Error("execute css", "err", err.Error(), header.GetRequestIDKV(ctx).Fuzzy())
+
+		return err
+	}
+
+	return c.next.Do(ctx, args)
+}
